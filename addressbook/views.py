51ac9daedb5115e24166d40d64de4ebacf9a7abd
Sean Gaw <gawntree.sean@gmail.com>
2019-03-15 18:29:45 +0800

    Third commit

--- addressbook/views.py
+++ addressbook/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
 from .models import AddressBookList
-
 
 @login_required
 def home(request):
